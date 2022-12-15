0e2ddb5ba6393950efe7cbad652b233b0187a9d9
vilmakilpelainen <n2kivi00@students.oamk.fi>
2022-12-15 09:28:05 +0200

    first commit

---
+++ code.js
@@ -0,0 +1,11 @@
+function calculateSum(number1, number2){
+    let sum = number1 + number2
+    return sum;
+}
+
+function main(){
+    document.write(calculateSum(1,2));
+}
+
+main();
+console.log(calculateSum(9,5));
